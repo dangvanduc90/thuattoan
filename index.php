2859c844b520ad4e416a6019b16f7d39530fedd9
dangvanduc90 <ducdangvan@vccorp.vn>
2018-12-15 00:20:52 +0700

    magic method

--- index.php
+++ index.php
@@ -71,11 +71,11 @@
  * Nguyen-Ly-Composition-over-Inheritance
  *  =============== SESSION 4 ===============
  */
-require_once ('MallardDuck.php');
-
-$duck = new MallardDuck();
-echo $duck->flyBehaviour->fly();
-echo $duck->flyBehaviour->is_check;
+//require_once ('MallardDuck.php');
+//
+//$duck = new MallardDuck();
+//echo $duck->flyBehaviour->fly();
+//echo $duck->flyBehaviour->is_check;
 
 ///*
 // * trait
@@ -117,3 +117,99 @@ echo $duck->flyBehaviour->is_check;
 //}
 //$user = new User();
 //echo $user->check();
+
+
+/*
+ * magic method
+ *  =============== SESSION 7 ===============
+ */
+
+class ClassName {
+    public $name = '';
+    public function __construct()
+    {
+        echo "__construct has been called<br>";
+    }
+    public function __set($key, $value)
+    {
+        echo "__set Setting $key to $value<br>";
+    }
+    public function __get($name)
+    {
+        echo "__get name is: $name<br>";
+    }
+    public function __isset($name)
+    {
+        echo "__isset name is: $name<br>";
+    }
+    public function __invoke(...$value)
+    {
+        echo "__invoke has been called ".json_encode($value)."<br>";
+    }
+    public function __unset($name)
+    {
+        echo "__unset name is: $name<br>";
+    }
+    // Phương thức __call() sẽ được gọi khi bạn gọi tới một phương thức nào đó không tồn tại trong đối tượng
+    public function __call($name, $arguments)
+    {
+        echo "__call has been called $name with ".json_encode($arguments)."<br>";
+    }
+    public static function __callStatic($name, $arguments)
+    {
+        echo "__callStatic has been called $name with ".json_encode($arguments)."<br>";
+    }
+    public function __toString()
+    {
+        // TODO: Implement __toString() method.
+        return "__toString has been called<br>";
+    }
+    public function __sleep()
+    {
+        echo "__sleep has been called<br>";
+        // Thông thường khi chúng ta serialize() một đối tượng thì nó sẽ trả về tất cả các thuộc tính trong đối tượng đó
+        //Nhưng nếu sử dụng __sleep() thì chúng ta có thể quy định được các thuộc tính có thể trả về.
+        return array('name');
+    }
+    public function __wakeup()
+    {
+        $this->name;
+        echo "__wakeup has been called<br>";
+    }
+    public function __clone()
+    {
+        echo "__clone has been called<br>";
+    }
+//    public static function __set_state($an_array)
+//    {
+//        echo "__set_state has been called with ".json_encode($an_array)."<br>";
+//    }
+    public function __debugInfo()
+    {
+        echo "__debugInfo has been called<br>";
+    }
+    public function __destruct()
+    {
+        echo "__destruct has been called<br>";
+    }
+
+    public static function testCall() {
+        return 'testCall';
+    }
+}
+
+$class = new ClassName(); // __construct
+$class->username = 'dangvanduc90'; // __set
+echo $class->username; // __get
+isset($class->username); // __isset
+$class('name', 'dangvanduc90'); // __invoke
+unset($class->username); // __unset
+echo $class->methodNotExist('dd'); // __call
+ClassName::username('da'); // __callStatic
+echo $class; // __toString
+$var = serialize($class); // Phương thức __sleep() sẽ được gọi khi chúng ta thực hiện serialize() đối tượng.
+//unserialize($var); // __wakeup also call __destruct too
+$class1 = clone $class; // __clone
+//var_export($class); // __set_state
+var_dump($class); // __debugInfo
+unset($class); // __destruct
