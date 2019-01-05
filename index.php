5764c3db2aea6f6c6e67f3479f97401905ab91fc
Đặng Văn Đức <ducdangvan@vccorp.vn>
2019-01-05 10:10:54 +0700

    builder design pattern

--- index.php
+++ index.php
@@ -804,3 +804,87 @@
 //$urlArr['sections'] = explode('/', $urlArr['path']);
 //$last = end($urlArr['sections']);
 //var_dump(dirname($url));
+
+
+/*
+ *  ============ SECTION 19 ============ */
+
+interface SQLQueryBuilder
+{
+    public function select(string $table, array $field) : SQLQueryBuilder;
+    public function where(string $field, string $value, string $operator = '') : SQLQueryBuilder;
+    public function limit(int $start, int $offset): SQLQueryBuilder;
+    public function getSQL(): string;
+}
+
+class MysqlQueryBuilder implements SQLQueryBuilder {
+
+    protected $query;
+
+    protected function reset() {
+        $this->query = new \stdClass;
+    }
+
+    public function select(string $table, array $field): SQLQueryBuilder
+    {
+        $this->reset();
+        $this->query->base = "select " . implode(",", $field) . " from " . $table;
+        $this->query->type = "select";
+
+        return $this;
+    }
+
+    public function where(string $field, string $value, string $operator = ''): SQLQueryBuilder
+    {
+        if (!in_array($this->query->type, ['select', 'update', 'delete'])) {
+            throw new Exception("WHERE can only be added to SELECT or UPDATE or DELETE");
+        }
+        $this->query->where[] = "$field $operator '$value'";
+
+        return $this;
+    }
+
+    public function limit(int $start, int $offset): SQLQueryBuilder
+    {
+        if (!in_array($this->query->type, ['select'])) {
+            throw new \Exception("LIMIT can only be added to SELECT");
+        }
+        $this->query->limit = " LIMIT " . $start . "," . $offset;
+
+        return $this;
+    }
+
+    public function getSQL(): string
+    {
+        $query = $this->query;
+        $sql = $query->base;
+        if (!empty($query->where)) {
+            $sql .= " WHERE " . implode(" AND ", $query->where);
+        }
+        if (isset($query->limit)) {
+            $sql .= $query->limit;
+        }
+        $sql .= ";";
+        return $sql;
+    }
+}
+
+class PostgresQueryBuilder extends MysqlQueryBuilder {
+    public function limit(int $start, int $offset): SQLQueryBuilder
+    {
+        parent::limit($start, $offset);
+        $this->query->limit = " LIMIT " . $start . " OFFSET " . $offset;
+
+        return $this;
+    }
+}
+
+function clientCode(SQLQueryBuilder $queryBuilder) {
+    $query = $queryBuilder
+        ->select("users", ["name", "email"])
+        ->where("age", 18, ">")
+        ->limit(0, 10)
+        ->getSQL();
+    echo $query;
+}
+clientCode(new MysqlQueryBuilder());
